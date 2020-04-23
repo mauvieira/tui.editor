ee3e3fbf393fb02f80a2c8f77d3dfe2f5c413989
seonim-ryu <seonim.ryu@nhn.com>
2020-04-24 01:01:55 +0900

    feat: change style name from editor-layout to editor-only

--- apps/editor/src/js/indexLayoutStyle.js
+++
@@ -1,8 +0,0 @@
-/**
- * @fileoverview entry point to extract old.css files
- * @author NHN FE Development Lab <dl_javascript@nhn.com>
- */
-
-import '../css/editor.css';
-import '../css/preview-highlighting.css';
-import '../css/md-syntax-highlighting.css';
